57f48f12750d3f5890b37bfdda5dd1c02f3ef74d
Arseni Dudko <arsenidudko@mail.ru>
2018-04-03 11:46:41 +0300

    Implemented directory and content displaying

--- controller.rb
+++ controller.rb
@@ -3,9 +3,7 @@ class LogParserController
 	def initialize
 		@log_file = LogFile.new
 		@current_view = FileDialogView.new
-		@current_view.clear_display
-		@current_view.set_cursor
-		@current_view.display
+		@current_view.display @log_file
 	end
 	def run
 		while user_input = $stdin.getch do
@@ -17,6 +15,7 @@ class LogParserController
 			rescue IO::WaitReadable
 			end
 			if @current_view.quittable? && user_input == 'q'
+				@current_view.turn_on_cursor
 				break
 			else
 				parse_input user_input
@@ -30,10 +29,19 @@ class LogParserController
                 	when "\n"
                             #change controller likely
                             #check the View's current interaction index to see what's next
+				
                         when "\e[A"
                            #up button ... update the view with an up action
+                            case @current_view.class.to_s 
+				when "FileDialogView"
+					file_dialog_move -1
+                            end
                         when "\e[B"
                             #down
+			    case @current_view.class.to_s
+                               when "FileDialogView"
+                               		file_dialog_move 1
+                            end
                         when "\e[C"
                             #right
                         when "\e[D"
@@ -44,4 +52,17 @@ class LogParserController
                 end
 
 	end
+
+# ---------  actions  ----------- #
+
+	def file_dialog_move increment
+		@log_file.directory_index += increment
+                if @log_file.directory_index < @log_file.list_start
+                      @log_file.list_start = @log_file.directory_index - $stdin.winsize[0] + 3
+                elsif @log_file.directory_index > @log_file.list_start + $stdin.winsize[0] - 3
+                      @log_file.list_start = @log_file.directory_index
+                end
+                @current_view.update @log_file
+	end
+
 end
--- view.rb
+++ view.rb
@@ -3,6 +3,14 @@ class BasicView
 		print "\e[2J"
 	end
 
+	def turn_off_cursor
+		print "\e[?25l"
+	end
+
+	def turn_on_cursor
+		print "\e[?25h"
+	end
+
 	def set_cursor row = 1, column = 1
 		print "\e[#{row};#{column}H"
 	end
@@ -17,14 +25,39 @@ class BasicView
 	def red text
 		"\e[31;40m#{text}\e[0m"
 	end
+
 end
 class FileDialogView < BasicView
-	def display
+	def display log_file
+		clear_display
+		turn_off_cursor
+		set_cursor
 		puts red(center("Select an Apache log file."))
+		update log_file
+	end
+	def update log_file
+		set_cursor 2, 1
+		log_file.directory.each_with_index do |directory_entry, index |
+                        if index < log_file.list_start
+                                next
+                        end
+                        if index > log_file.list_start + $stdin.winsize[0] - 3
+                                break
+                        end
+                        directory_entry = directory_entry + "/" if Dir.exist?(log_file.file_path + directory_entry)
+                        directory_entry = red(directory_entry) if index == log_file.directory_index
+                        print directory_entry + "\e[K\n"
+                        
+		end
+		print "\e[J"
+		set_cursor $stdin.winsize[0], 1
+		print red("Type 'q' to exit; up/down to move; return to select")
+              
 	end
 	def quittable?
 		true
 	end
+
 end
 
 class LogListView < BasicView
